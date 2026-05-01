d3424ea996c5954f809a2c5e81cbba241691ec61
Samii3. <saramichelcontrerascaceres22@gmail.com>
2026-05-01 00:37:57 -0500

    Add variable definitions and type prints in variable.py

---
+++ variable.py
@@ -0,0 +1,17 @@
+# Esto es un comentario
+# de varias líneas
+
+nombre = "Sara Michel Contreras Caceres"
+edad = 14
+estado = True
+nota = 45.0
+
+print(type(nombre))
+print(type(edad))
+print(type(estado))
+print(type(nota))
+
+print(nombre)
+print(edad)
+print(estado)
+print(nota)
